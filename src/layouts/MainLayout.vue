7eb4db7ee1835665c21acec8c0de77e839bdbe6a
maximusdev1313 <maximusdev1313@gmail.com>
2022-04-29 15:11:31 +0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,207 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+<!-- this is header -->
+    <q-header
+      elevated
+      class="bg-white text-dark">
+      <q-toolbar>
+       <q-btn
+        danse
+        flat
+        round
+        icon="menu"
+        @click="toggleLeftDrawer"
+        ></q-btn>
+
+        <q-toolbar-title
+          class="text-weight-bold gt-sm"
+          >
+
+            {{$route.name}}
+        </q-toolbar-title>
+        <q-icon
+          name="fas fa-dove"
+          size="sm"
+          class="q-pa-md lt-md absolute-center"
+          color="primary"
+
+      > </q-icon>
+      </q-toolbar>
+    </q-header>
+<!-- this is left drawer -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      class="absolute"
+      >
+
+      <!-- for icon -->
+      <span class="row justify-between items-center">
+         <q-icon name="fas fa-dove"
+            size="lg"
+            class="q-pa-lg "
+            color="primary"
+
+      > </q-icon>
+      <q-icon
+        name="fa fa-arrow-left"
+        size="sm"
+        class="q-mr-md lt-md"
+        @click="toggleLeftDrawer"
+        rounded
+        clickable
+      >
+      </q-icon>
+      </span>
+
+      <q-list >
+          <q-item
+            clickable
+            v-ripple
+            to="/"
+            exact
+            >
+              <q-item-section avatar>
+                 <q-icon
+                    name="home"
+                    size="md" />
+              </q-item-section>
+              <q-item-section
+                  class="text-weight-bold">Home
+              </q-item-section>
+          </q-item>
+          <!-- <q-item
+            exact
+            clickable
+            v-ripple
+            to="/about">
+            <q-item-section
+                avatar>
+                 <q-icon
+                    name="help"
+                    size="md" />
+              </q-item-section>
+              <q-item-section
+                class="text-weight-bold">About
+              </q-item-section>
+
+          </q-item> -->
+           <!-- <q-item
+            clickable
+            v-ripple
+            to="/signin"
+            exact
+            >
+              <q-item-section avatar>
+                 <q-icon
+                    name="login"
+                    size="md" />
+              </q-item-section>
+              <q-item-section
+                  class="text-weight-bold">
+                  SignIn
+              </q-item-section>
+          </q-item>
+            <q-item
+            clickable
+            v-ripple
+            to="/register"
+            exact
+            >
+              <q-item-section avatar>
+                 <q-icon
+                    name="person_add"
+                    size="md" />
+              </q-item-section>
+              <q-item-section
+                  class="text-weight-bold">
+                  Register
+              </q-item-section> 
+          </q-item>-->
+
+      </q-list>
+
+    </q-drawer>
+  <!-- this is right drawer -->
+   <!-- <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      bordered>
+      for search field
+       <q-input
+          color="primary"
+          outlined
+          rounded
+          danse
+          label="Search"
+          class="q-ma-md"
+          >
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+
+         <q-list separator>
+            <q-item >
+              <q-item-section>
+
+                <q-item-label class="text-grey" overline>Education</q-item-label>
+                 <q-item-label class="text-black text-weight-bold" >Something amazing happend</q-item-label>
+                <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>5 min ago</q-item-label>
+                <q-icon name="star" color="yellow" />
+              </q-item-section>
+            </q-item>
+                        <q-item >
+              <q-item-section>
+
+                <q-item-label class="text-grey" overline>Education</q-item-label>
+                 <q-item-label class="text-black text-weight-bold" >Something amazing happend</q-item-label>
+                <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>5 min ago</q-item-label>
+                <q-icon name="star" color="yellow" />
+              </q-item-section>
+            </q-item>
+
+      </q-list>
+
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,227 @@
+<template>
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute full-height full-width">
+      
+      
+      <!-- tweet lists -->
+        <q-list>
+          <q-item class="q-mt-md" v-for="tweet in tweets" :key="tweet.id">
+           <q-item-section avatar top>
+              <q-avatar>
+                <q-icon name="person" size="lg" color="grey"/>
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-weigth-bold text-subtitle1">
+                <span class="text-grey">
+                  @{{tweet.name}}
+                  
+                  <br class="lt-md">
+                  <span class="text-subtitle2">
+                      <!-- {{ moment(date).format(" MMM DD, YYYY [at] HH:mm a") }} -->
+                  </span>
+                </span>
+                </q-item-label>
+              <q-item-label class="inputForTweet">
+                {{tweet.massage}}
+              </q-item-label>
+              <div class="row justify-between">
+                <q-btn
+                color="grey"
+                icon="far fa-comment"
+                size="sm"
+                flat
+                round
+                ></q-btn>
+                <!-- <q-btn
+                color="grey"
+                icon="fas fa-retweet"
+                size="sm"
+                flat
+                round
+                ></q-btn> -->
+                <!-- <q-btn
+                @click="toggleLiked(tweet)"
+                :color="tweet.liked ? 'pink' : 'grey' "
+                :icon="tweet.liked ? 'fas fa-heart' : 'far fa-heart'"
+                size="sm"
+                flat
+                round
+                >
+                &bull; {{tweet.likes}}
+                </q-btn>
+
+                <q-btn
+                color="grey"
+                icon="fas fa-trash"
+                size="sm"
+                flat
+                round
+                @click="removeTweet(tweet)"
+                ></q-btn> -->
+              </div>
+
+
+            </q-item-section>
+          </q-item>
+
+        <q-separator inset="item" />
+
+
+
+      </q-list>
+      <q-btn color="accent" @click="toggleQuestionPanel" v-if="!openQuestionPanel" class="relative-position">
+        Savol berish
+      </q-btn>
+      <div class="row items-end q-col-glutter-md inputs " v-if="openQuestionPanel">
+        <div class="col">
+          <q-input
+          bottom-slots
+          v-model="name"
+          label="Ismingiz"
+          counter
+          maxlength="280"
+          class="inputForTweet q-py-md q-px-sm newTweet"
+          autogrow
+
+          >
+
+          <template v-slot:before>
+            <q-avatar size="md">
+              <q-icon name="person" size="md"/>
+            </q-avatar>
+          </template>
+
+          <template v-slot:append>
+            <!-- <q-icon v-if="newTweet !== ''" name="close" @click="newTweet = ''" class="cursor-pointer" /> -->
+
+          </template>
+
+        </q-input>
+         <q-input
+          bottom-slots
+          v-model="massage"
+          label="Savol bering"
+          counter
+          maxlength="280"
+          class="inputForTweet q-py-lg q-px-md newTweet"
+          autogrow
+          v-if="name.length >= 3"
+          >
+           <template v-slot:append>
+            <q-icon v-if="massage !== ''" name="close" @click="massage = ''" class="cursor-pointer" />
+
+          </template>
+          </q-input> 
+        </div>
+        <div class="col col-shrink">
+
+             <q-btn
+              @click="addTweet"
+              :disable="!addTweet"
+              color="primary"
+              class="q-mb-md q-mr-md"
+              label="Yuborish"
+              rounded
+              unelevated
+              no-caps
+              /> 
+
+        </div>
+      </div>
+    </q-scroll-area>
+  
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, onMounted,ref } from 'vue';
+import axios from 'axios'
+import Pusher from 'pusher-js'
+// import { formatDistance} from 'date-fns'
+import moment from 'moment'
+export default defineComponent({
+  name: 'Index',
+  
+  setup(){
+    const openQuestionPanel = ref(false)
+    const name  = ref('')
+    const massage = ref('')
+    const tweets = ref([])
+
+   
+    onMounted(()=>{
+       Pusher.logToConsole = false;
+
+      const pusher = new Pusher('8ac282d8b5fbf6b803e7', {
+        cluster: 'ap2'
+      });
+
+      const channel = pusher.subscribe('TweetMax-development');
+      channel.bind('massage', data => {
+        tweets.value.push(data);
+    });
+
+    })
+    const addTweet = async ()=>{
+      await fetch('http://maximusdev.pythonanywhere.com/api/', {
+        
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          name: name.value,
+          massage: massage.value,
+        }) 
+      })
+      massage.value = ''
+    }
+     const getTweet = async ()=>{
+      try {
+        const res = await axios.get('http://maximusdev.pythonanywhere.com/api/')
+        tweets.value = res.data
+        
+             }
+      catch(err){
+        console.log(err);
+      }
+    }
+    setInterval(()=>{
+      getTweet()
+    }, 100)
+
+    return{
+       tweets,
+       name,
+       massage,
+       addTweet,
+       openQuestionPanel,
+        toggleQuestionPanel(){
+          openQuestionPanel.value = !openQuestionPanel.value
+        }
+
+       
+    }
+  }
+
+
+})
+</script>
+<style lang="sass">
+.inputForTweet
+    white-space: pre-line
+.newTweet
+    textarea
+      font-size: 16px
+      line-height: 1.4 !important
+.devider
+    border-top: 1px solid
+    border-bottom: 1px solid
+    border-color: $grey-5
+    margin-top: 50px
+.inputs
+   
+    background-color: #fff
+    height: auto
+</style>
